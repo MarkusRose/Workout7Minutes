0d0893ffa8f357c13cc760866a72b8b79f60fa3b
Markus Rose <markus.m.rose@gmail.com>
2026-05-10 16:15:17 +0200

    feat(UpgradeAngular): upgrade to angular 21

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { SidebarComponent } from '../components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterModule, SidebarComponent],
+  imports: [RouterModule, SidebarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class App { }
+export class App {}
--- src/components/sidebar/sidebar.component.ts
+++ src/components/sidebar/sidebar.component.ts
@@ -1,16 +1,15 @@
-import { CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, Component, inject, signal } from "@angular/core";
-import { toSignal } from "@angular/core/rxjs-interop";
-import { Router } from "@angular/router";
-import { WorkoutSessionService } from "../../services/workout-session.service";
-import { tap, map } from "rxjs";
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Router } from '@angular/router';
+import { WorkoutSessionService } from '../../services/workout-session.service';
+import { tap, map } from 'rxjs';
 
 @Component({
   selector: 'workout-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule],
+  imports: [],
 })
 export class SidebarComponent {
   public readonly modes: ModeItem[] = [
@@ -28,13 +27,16 @@ export class SidebarComponent {
 
   private readonly router = inject(Router);
   private readonly workoutService = inject(WorkoutSessionService);
-  public readonly workoutList = toSignal(this.workoutService.workoutList$.pipe(tap((list) => console.log(list))));
-  public readonly selectedWorkoutName = toSignal(this.workoutService.currentWorkout$.pipe(map((workout) => workout.name)));
-  public readonly activeModeName = signal(this.modes[0].name)
-
+  public readonly workoutList = toSignal(
+    this.workoutService.workoutList$.pipe(tap((list) => console.log(list))),
+  );
+  public readonly selectedWorkoutName = toSignal(
+    this.workoutService.currentWorkout$.pipe(map((workout) => workout.name)),
+  );
+  public readonly activeModeName = signal(this.modes[0].name);
 
   public redirectTo(mode: ModeItem): void {
-    this.activeModeName.set(mode.name)
+    this.activeModeName.set(mode.name);
     this.router.navigateByUrl(`/${mode.route}`);
   }
 
@@ -48,4 +50,3 @@ interface ModeItem {
   icon: string;
   route: string;
 }
-
